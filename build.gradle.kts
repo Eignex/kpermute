5f6a39597f0b939a04c6ffad725402010eaa832c
Rasmus Ros <rasmus.ros@theca.com>
2026-04-08 11:21:06 +0200

    chore: update to com.eignex.kmp plugin version 1.1.1 and expand target platforms

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    id("com.eignex.kmp") version "1.0.0"
+    id("com.eignex.kmp") version "1.1.1"
 }
 
 eignexPublish {
     description.set(
-        "Kotlin library for shuffling lists too big for memory or for ID obfuscation. " + "Using bijective integer permutations with fast cycle-walking hash mixing."
+        "Kotlin library for shuffling lists too big for memory or for ID obfuscation. " +
+                "Using bijective integer permutations with fast cycle-walking hash mixing."
     )
     githubRepo.set("Eignex/kpermute")
 }
@@ -13,5 +14,9 @@ eignexPublish {
 kotlin {
     jvm()
     js(IR) { browser(); nodejs() }
-    linuxX64(); macosX64(); macosArm64(); mingwX64()
+    wasmJs { browser(); nodejs() }
+    wasmWasi { nodejs() }
+    linuxX64(); linuxArm64()
+    macosX64(); macosArm64(); mingwX64()
+    iosX64(); iosArm64(); iosSimulatorArm64()
 }
